d2eecf13a5c00320e269c41da146c095869735e4
Nektarios <nektario@gmail.com>
2019-08-02 12:57:58 -0400

    Create and manage myList

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useReducer, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { MyListProvider } from './MyListContext'
+import usePersistentState from './hooks/usePersistentState'
 import Home from './views/Home'
 import axios from 'axios'
 
@@ -34,14 +36,22 @@ const featuredMovies = [
 
 function App() {
     const [allMovies, setAllMovies] = useState([])
+    const [myList, setMyList] = usePersistentState(useState([]))
+    const MyList = {
+        list: myList,
+        update: handleMyListUpdate,
+        has: (movie) => isMovieInMyList(movie)
+    }
     const randomFeatureMovie = featuredMovies[Math.floor(Math.random() * featuredMovies.length)]
-    const featureMovie = allMovies
-                            .filter(movie => movie.uid === randomFeatureMovie.uid)
-                            .map(movie => ({ ...movie, ...randomFeatureMovie}))[0]
+    const featureMovie = useRef()
+    if (!featureMovie.current) {
+        featureMovie.current = allMovies
+                .filter(movie => movie.uid === randomFeatureMovie.uid)
+                .map(movie => ({ ...movie, ...randomFeatureMovie}))[0]
+    }
 
     useEffect(() => {
-        const url = '/data/movies.json'
-        axios.get(url)
+        axios.get('/data/movies.json')
             .then(response => {
                 if (response.data) {
                     setAllMovies(response.data.items)
@@ -50,12 +60,35 @@ function App() {
             .catch(e => console.log(e))
     }, [])
 
+    function handleMyListUpdate(movie) {
+        let newList
+
+        if (isMovieInMyList(movie)) {
+            newList = myList.filter(myListMovie => myListMovie.uid !== movie.uid)
+        } else {
+            newList = [{ uid: movie.uid, dateAdded: Date.now() }].concat(myList)
+        }
+        
+        setMyList(newList)
+    }
+
+    function isMovieInMyList(movie) {
+        for (const myListMovie of myList) {
+            if (myListMovie.uid === movie.uid) {
+                return true
+            }
+        }
+        return false
+    }
+
     if (allMovies.length === 0) {
         return null
     }
 
     return (
-        <Home allMovies={allMovies} feature={featureMovie} />
+        <MyListProvider value={MyList}>
+            <Home allMovies={allMovies} myList={myList} feature={featureMovie.current} />
+        </MyListProvider>
     )
 }
 
