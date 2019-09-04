4d75eb8f5f5e46608aaf8d6ec3e095d1b943560c
Nektarios <nektario@gmail.com>
2019-09-04 11:47:03 -0400

    Refactor: Remove unused code

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,62 +1,12 @@
-import React, { useReducer, useState } from 'react'
+import React, { useState } from 'react'
 import { useMyListHelper } from '../my-list-context'
-import usePersistentState from '../hooks/usePersistentState'
-import { useTransition, animated, config } from 'react-spring'
 import Header from '../components/Header'
-import Movie from '../Movie'
 import moment from 'moment'
-import MovieFilters from '../models/MovieFilters'
-import Slider from '../components/Slider/Slider'
 import MovieSliderRow from '../components/MovieSliderRow'
 import Feature from '../components/Feature'
-import Backdrop from '../components/Backdrop'
-import ConditionalRender from '../components/ConditionalRender'
-import ConditionalRenderFade from '../components/ConditionalRenderFade'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faVideo } from '@fortawesome/free-solid-svg-icons'
 import './Home.scss'
 
-const ROW_DETAILS_HEIGHT = '30vw'
-
-function movieFilterReducer(state, action) {
-    function sortByTitle(a, b) {
-        return a.title.localeCompare(b.title)
-    }
-
-    switch (action.type) {
-        case 'initial-movie-load' :
-            return { ...state, allMovies: action.payload, moviesToDisplay: action.payload.sort(sortByTitle), currentSort: ['sort-title-asc']}
-        case 'sort-title-asc' :
-            return { ...state, moviesToDisplay: state.moviesToDisplay.sort(sortByTitle), currentSort: ['sort-title-asc'] }
-        case 'sort-title-desc' :
-            return { ...state, moviesToDisplay: state.moviesToDisplay.sort((a, b) => sortByTitle(b, a)), currentSort: ['sort-title-desc'] }
-        case 'sort-date-added' :
-            return { ...state, moviesToDisplay: state.moviesToDisplay.sort((a, b) => b.metadata.created_date_millis - a.metadata.created_date_millis), currentSort: ['sort-date-added'] }
-        case 'sort-release-year' :
-            return { ...state, moviesToDisplay: state.moviesToDisplay.sort((a, b) => b.year - a.year), currentSort: ['sort-release-year'] }
-        case 'sort-imdb-rating' :
-            return { ...state, moviesToDisplay: state.moviesToDisplay.sort((a, b) => b.imdb.rating - a.imdb.rating), currentSort: ['sort-imdb-rating'] }
-        case 'filter-seen' : {
-            if (state.movieFilters.has(action)) {
-                state.movieFilters.remove(action)
-            } else {
-                state.movieFilters.add(action)
-            }
-
-            return { ...state, moviesToDisplay: state.movieFilters.applyFilter(state.allMovies), movieFilters: Object.assign({}, state.movieFilters) }
-        }
-        default :
-            return state
-    }
-}
-
 function Home(props) {
-    const [state, dispatch] = useReducer(movieFilterReducer, {
-        allMovies: [],
-        moviesToDisplay: [],
-        currentSort: [],
-        movieFilters: MovieFilters()
-    })
     const [currentlyOpenedMovieDetailsRow, setCurrentlyOpenedMovieDetailsRow] = useState()
     const myListHelper = useMyListHelper()
     const rows = [
@@ -84,7 +34,8 @@ function Home(props) {
         }
     ]
 
-    function toggleMovieDetailsVisibility(titleOfClickedRow, clickedMovie) {
+
+    function toggleMovieDetailsVisibility(titleOfClickedRow) {
         if (titleOfClickedRow === currentlyOpenedMovieDetailsRow) {
             setCurrentlyOpenedMovieDetailsRow('')
         } else {
@@ -98,23 +49,7 @@ function Home(props) {
 
     return (
         <div className='home'>
-            {/* <Header
-                dispatch={dispatch}
-                currentSort={state.currentSort}
-                currentFilter={state.currentFilter}
-                totalMovies={state && state.allMovies.length}
-                displayedMovies={state.moviesToDisplay.length}
-            /> */}
-
             <Header />
-
-            {/* <div className='video'>
-                <div className='video-background'>
-                    <div className='video-foreground'>
-                        <iframe src={`${getFeatureVideoLink(state.allMovies[350].trailer)}`} frameBorder="0" allowFullScreen allow='autoplay'></iframe>
-                    </div>
-                </div>
-            </div> */}
 
             <div id='feature'>
                 <Feature
@@ -124,12 +59,11 @@ function Home(props) {
             </div>
 
             <main>
-            {/* { rows.filter(row => row.movies.length > 0).map(row =>  */}
                 { rows.map(row => 
                     <MovieSliderRow
                         key={row.title}
                         rowTitle={row.title}
-                        onMovieDetailsClick={(title, movie) => toggleMovieDetailsVisibility(title, movie)}
+                        onMovieDetailsClick={(title) => toggleMovieDetailsVisibility(title)}
                         shouldOpen={currentlyOpenedMovieDetailsRow === row.title}
                         movies={row.movies}
                     />)
