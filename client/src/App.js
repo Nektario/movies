58a359adac3a369e8d5bc5bd006ba256b5f772e7
Nektarios <nektario@gmail.com>
2019-09-04 09:17:10 -0400

    Refactor: extract out featuremovie data

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { MyListProvider } from './MyListContext'
 import usePersistentState from './hooks/usePersistentState'
+import featuredMovies from './data/feature-movies'
 import Home from './views/Home'
 import axios from 'axios'
-
-const featuredMovies = [
-    { 
-        uid: '401981',
-        title: 'Red Sparrow',
-        featureLogo: 'logo_red_sparrow.png',
-        featureTrailer: {
-            name: 'red_sparrow_trailer.mp4',
-            mimeType: 'video/mp4'
-        }
-    },
-    { 
-        uid: '284053',
-        title: 'Thor: Ragnarok',
-        featureLogo: 'logo_thor_ragnarok.png',
-        featureTrailer: {
-            name: 'thor_ragnarok_trailer.mp4',
-            mimeType: 'video/mp4'
-        }
-    },
-    { 
-        uid: '383498',
-        title: 'Deadpool 2',
-        featureLogo: 'logo_deadpool2.png',
-        featureTrailer: {
-            name: 'deadpool_2_trailer.mp4',
-            mimeType: 'video/mp4'
-        }
-    }
-]
 
 function App() {
     const [allMovies, setAllMovies] = useState([])
---
+++ client/src/data/feature-movies.js
@@ -0,0 +1,29 @@
+export default [
+    { 
+        uid: '401981',
+        title: 'Red Sparrow',
+        featureLogo: 'logo_red_sparrow.png',
+        featureTrailer: {
+            name: 'red_sparrow_trailer.mp4',
+            mimeType: 'video/mp4'
+        }
+    },
+    { 
+        uid: '284053',
+        title: 'Thor: Ragnarok',
+        featureLogo: 'logo_thor_ragnarok.png',
+        featureTrailer: {
+            name: 'thor_ragnarok_trailer.mp4',
+            mimeType: 'video/mp4'
+        }
+    },
+    { 
+        uid: '383498',
+        title: 'Deadpool 2',
+        featureLogo: 'logo_deadpool2.png',
+        featureTrailer: {
+            name: 'deadpool_2_trailer.mp4',
+            mimeType: 'video/mp4'
+        }
+    }
+]
