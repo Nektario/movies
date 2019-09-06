9fcbf2192b0c3e06b74f78db8af2e5414b690b03
Nektarios <nektario@gmail.com>
2019-09-06 11:42:43 -0400

    Refactor/simplify styles

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -63,7 +63,7 @@ function Header(props) {
 
     return (
         <div className='header'>
-            <div className='top-scrim'></div>
+            <div className='top-scrim' />
 
             <div className='header-content'>
                 <div className='left'>
@@ -73,8 +73,8 @@ function Header(props) {
                             <path fill="#fff" d="M361.5 239.2L173.9 108.6c-9.5-7.3-30.8-4.8-32.1 16.8v261.2c1.5 23.3 24 23.3 32.1 16.8l187.6-130.6c8.1-5.3 15.7-20.9 0-33.6zM182.6 347.5v-183L314.1 256l-131.5 91.5z"/>
                         </svg>
                     </div>
-                    <div className='header-text-item'><Link to='/home'>Home</Link></div>
-                    <div className='header-text-item'><Link to='/my-list'>My List</Link></div>
+                    <Link to='/home'>Home</Link>
+                    <Link to='/my-list'>My List</Link>
                 </div>
 
                 <div className='right'>
